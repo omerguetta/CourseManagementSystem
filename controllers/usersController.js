cc414b836f8d7997ac4f3ee95e94ce6c8e9bd14b
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-01-03 17:23:59 +0200

    handle user login and add this to the Usercontroller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -60,6 +60,42 @@ const registerUser = async (req, res) => {
     }
 };
 
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { userId: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: "10m" }
+        );
+
+        res.status(200).json({
+            message: "Login successful!",
+            token,
+            user: {
+                userID: user._id,
+                fullName: user.fullName,
+                role: user.role,
+            },
+        });
+    } catch (error) {
+        console.error("Error logging in user:", error.message);
+        res.status(500).json({ error: "Failed to log in user" });
+    }
+};
+
 module.exports = {
     registerUser,
+    loginUser,
 };
