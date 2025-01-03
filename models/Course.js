cd510e89f1dfe08420aedd0ac22bddf46bbf2558
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-01-03 23:58:10 +0200

    create course schema

---
+++ models/Course.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require("mongoose");
+
+const courseSchema = new Schema({
+    name: { type: String, required: true },
+    instructor: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    creditPoints: { type: Number, required: true, min: 3, max: 5 },
+    maxStudents: { type: Number, required: true },
+    registeredStudents: [{ type: Schema.Types.ObjectId, ref: "Student" }],
+}, { timestamps: true });
+
+const Course = model("Course", courseSchema);
+module.exports = Course;
