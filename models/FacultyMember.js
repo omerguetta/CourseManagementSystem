31c6ca1d9a81e48b8003fe214164a0e6b51fbcd0
omerguetta <omer.guetta@stu.shenkar.ac.il>
2025-01-04 12:33:50 +0200

    add array of course

--- models/FacultyMember.js
+++ models/FacultyMember.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 const facultySchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     department: { type: String, required: false },
+    courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
 });
 
 module.exports = model("FacultyMember", facultySchema, "staff");
